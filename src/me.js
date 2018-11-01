34d93f62306ecf9b873ae4b82e0be001028d59fa
shoheiono <wa2.a.nek0ch+git@gmail.com>
2018-11-02 00:39:28 +0900

    use a service

--- src/me.js
+++ src/me.js
@@ -1,11 +1,12 @@
 const request = require('superagent');
-const { BASE_URI } = require('./constants');
+const { BASE_URI, CHATWORK_TOKEN } = require('./constants');
 const { requestSuccess, requestError } = require('./service');
+const BASE_ME_URI = `${BASE_URI}me`;
 
 async function get(apiToken) {
     return request
-        .get(`${BASE_URI}me`)
-        .set('X-ChatWorkToken', apiToken)
+        .get(`${BASE_ME_URI}`)
+        .set(CHATWORK_TOKEN, apiToken)
         .then(requestSuccess)
         .catch(requestError);
 }
--- src/my.js
+++ src/my.js
@@ -1,21 +1,26 @@
 const request = require('superagent');
-const { BASE_URI } = require('./constants');
-const { requestSuccess, requestError } = require('./service');
+const { BASE_URI, CHATWORK_TOKEN } = require('./constants');
+const { requestSuccess, requestError, objectToQuery } = require('./service');
+const BASE_MY_URI = `${BASE_URI}my`;
 
-async function statusGet(apiToken) {
-    return request
-        .get(`${BASE_URI}my/status`)
-        .set('X-ChatWorkToken', apiToken)
-        .then(requestSuccess)
-        .catch(requestError);
+const status = {
+    get: async (apiToken) => {
+        return request
+            .get(`${BASE_MY_URI}/status`)
+            .set(CHATWORK_TOKEN, apiToken)
+            .then(requestSuccess)
+            .catch(requestError);
+    }
+};
+
+const tasks = {
+    get: async (apiToken, options) => {
+        return request
+            .get(`${BASE_MY_URI}/tasks${objectToQuery(options)}`)
+            .set(CHATWORK_TOKEN, apiToken)
+            .then(requestSuccess)
+            .catch(requestError);
+    }
 }
 
-async function tasksGet(apiToken, assignedByAccountId, status) {
-    return request
-        .get(`${BASE_URI}my/status?assigned_by_account_id=${assignedByAccountId}&status=${status}`)
-        .set('X-ChatWorkToken', apiToken)
-        .then(requestSuccess)
-        .catch(requestError);
-}
-
-module.exports = { statusGet, tasksGet };
+module.exports = { status, tasks };
