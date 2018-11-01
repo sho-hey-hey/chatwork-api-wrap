43ebbd4eb9e8a15857c8db0ec4ee395fada7c55f
shoheiono <wa2.a.nek0ch+git@gmail.com>
2018-11-02 00:38:35 +0900

    add service

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,5 @@
 module.exports = {
+    CHATWORK_TOKEN: 'X-ChatWorkToken',
     BASE_URI: 'https://api.chatwork.com/v2/',
     STATUS_CODE_200: 200,
 };
--- src/service.js
+++ src/service.js
@@ -10,4 +10,22 @@ const requestError = (err) => {
     return err;
 }
 
-module.exports = { requestSuccess, requestError };
+const objectToQuery = (obj) => {
+    const keys = Object.keys(obj);
+    const query = keys
+        .filter((k) => obj[k] !== undefined)
+        .map((k) => {
+            const value = obj[k];
+            const v = typeof value === 'boolean' ? +value : value;
+            return `${k}=${v}`
+        })
+        .join('&');
+
+    return `?${query}`;
+}
+
+module.exports = {
+    requestSuccess,
+    requestError,
+    objectToQuery,
+};
