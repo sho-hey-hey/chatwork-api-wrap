72a41dd745d8c3180e2dced13cc3578d68b2e7cc
holyshared <holy.shared.design@gmail.com>
2019-02-05 16:11:07 +0900

    fix return

--- src/ts/request.ts
+++ src/ts/request.ts
@@ -2,7 +2,6 @@ import request from "superagent";
 import { AUTHORIZATION_TOKEN, CHATWORK_TOKEN } from "./constants";
 
 export const withToken = (apiToken: string) => (request: request.Request) : request.Request => {
-    request.set(CHATWORK_TOKEN, apiToken)
+    return request.set(CHATWORK_TOKEN, apiToken)
         .set(AUTHORIZATION_TOKEN, `Bearer ${apiToken}`);
-    return request;
 };
